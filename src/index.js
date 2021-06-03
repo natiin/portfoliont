04b474c4de383e5451ba6ead954e2e27103c4513
Natalia <nat.targowska@gmail.com>
2021-06-03 21:32:55 +0100

    fix path url for react router

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={"portfoliont"}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
